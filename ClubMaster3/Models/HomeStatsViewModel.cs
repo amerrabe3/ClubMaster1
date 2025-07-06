b50646abdd9a075e7cdc9a7bc4b43c203061810a
amerrabe3 <amrabi21@cit.just.edu.jo>
2025-07-06 12:19:58 +0300

    final complete match schedualing

--- ClubMaster3/Models/HomeStatsViewModel.cs
+++ ClubMaster3/Models/HomeStatsViewModel.cs
@@ -7,7 +7,10 @@ namespace ClubMaster3.Models
         public int TeamsCount { get; set; }
         public int PlayersCount { get; set; }
         public int CoachesCount { get; set; }
-
+        public int MatchCount { get; set; }
+        
+        
+        
         public List<Player> Players { get; set; }
         public List<Coach> Coaches { get; set; }
         public List<Team> Teams { get; set; }
